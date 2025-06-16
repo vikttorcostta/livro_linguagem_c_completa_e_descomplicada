4a0d0d88917d2aceb0ab60e1cd18d46601751138
Acer-Victor-Costa <victorfotografia12@gmail.com>
2025-06-16 15:55:44 -0300

    exercício_005 concluído

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 /*EXERCÍCIO 001
 Elabore um programa que escreva as mensagens "Início do programa" e "Fim do programa" na tela,
 uma em cada linha, usando apenas um comando print().*/
-
 void exercicio_01(){
     printf("Início do programa\n");
     printf("Fim do programa\n");
@@ -28,7 +25,6 @@ void exercicio_03(){
     scanf("%d", &numero);
     printf("O valor lido foi: %d", numero);
 }
-
 /*EXERCÍCIO 004
 Escreva um programa que leia um número inteiro e depois o imprima usando o operador "%f". Veja o que aconteceu.*/
 void exercicio_04(){
@@ -37,23 +33,31 @@ void exercicio_04(){
     printf("%f", numero);
 }
 
+/*EXERCÍCIO 005
+Faça um programa que leia um valor do tipo float e depois o imprima usando o operador "%d". Veja oque aconteceu.*/
+void exercicio_005(){
+    int numero;
+    scanf("%f", &numero);
+    printf("%d", numero);
+}
+
 int main()
 {
 
+    // concluído
+    // exercicio_01();
 
     // concluído
-    //exercicio_01();
+    // exercicio_02();
 
     // concluído
-    //exercicio_02();
+    // exercicio_03();
 
     // concluído
-    //exercicio_03();
+    // exercicio_04(); retornou 0.000000
 
-    //comcluído
-    exercicio_04();
-
-
+    // concluído
+    // exercicio_005(); retornou 1102053376
 
     system("pause");
     return 0;
